f022a47c7e5e596994f4e57592c166c86618ccf7
fujinlong-AX <1160893962@qq.com>
2020-08-03 17:12:03 +0800

    875

--- 875/875.js
+++ 875/875.js
@@ -5,11 +5,14 @@
  * @return {number}
  */
 var minEatingSpeed = function (piles, H){
-  let lo = 1
+  let K = 1
     hi = Math.max(...piles)
-  console.log(lo, hi);
+  // console.log(lo, hi);
+  for(K = 1;k <= H; hi--){
+    
+  }
   // 求出最大香蕉数， 在某堆中
-  return lo
+  return K
 }
 
 minEatingSpeed([3,6,7,11], 8)
