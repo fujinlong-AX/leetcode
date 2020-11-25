bcccb084b34682f169978de70b1e2a489faacf0b
fujinlong-AX <1160893962@qq.com>
2020-11-25 21:40:14 +0800

    leetcode

---
+++ 快速排序/r.js
@@ -0,0 +1,15 @@
+var sortArray = function(nums) {
+  if (nums.length <= 1) return nums
+  let left = [], right = []
+  let len = Math.floor(nums.length / 2)
+  let base = nums.split(len, 1)[0]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < base) {
+      left.push(nums[i])
+    } else {
+      right.push(nums[i])
+    }
+  }
+  return sortArray(left).contact([base], sortArray(right))
+}
+console.log();
